f7616ca7bbe99d7ed36aa79e1da71492c4723b46
Nir Tal <nirt04@gmail.com>
2020-03-29 04:49:25 +0300

    deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-/* eslint-disable */
-export const eventBus = new Vue();
-
-Vue.config.productionTip = false
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
